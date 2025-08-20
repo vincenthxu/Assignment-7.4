fdd24ebfcdb520459d908850f8dbc508b9ecd4ba
Vincent Xu <vincent.xu1@outlook.com>
2025-08-20 13:51:00 -0700

    Add project files.

---
+++ ParkingSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7._4
+{
+    public class ParkingSystem(int big, int medium, int small)
+    {
+        private int bigSpots = big;
+        private int mediumSpots = medium;
+        private int smallSpots = small;
+
+        public bool AddCar(int carType)
+        {
+            switch (carType)
+            {
+                case 1:
+                    return bigSpots-- > 0;
+                case 2:
+                    return mediumSpots-- > 0;
+                case 3:
+                    return smallSpots-- > 0;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment_7._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingSystem parkingSystem = new(1, 1, 0);
+            Console.WriteLine(parkingSystem.addCar(1)); // return true because there is 1 available slot for a big car
+            Console.WriteLine(parkingSystem.addCar(2)); // return true because there is 1 available slot for a medium car
+            Console.WriteLine(parkingSystem.addCar(3)); // return false because there is no available slot for a small car
+            Console.WriteLine(parkingSystem.addCar(1)); // return false because there is no available slot for a big car. It is already occupied
+        }
+    }
+}
